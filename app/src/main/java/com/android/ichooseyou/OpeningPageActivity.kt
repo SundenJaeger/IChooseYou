d0fa70294cdf46e0a92314f81ec5226c6f97ed82
drew <ralphandrew2004@gmail.com>
2025-03-16 03:37:11 +0800

    changed login,register UI added opening page ma not responding kung muadtog register ambot ngano

--- app/src/main/java/com/android/ichooseyou/OpeningPageActivity.kt
+++ app/src/main/java/com/android/ichooseyou/OpeningPageActivity.kt
@@ -1,8 +1,10 @@
 package com.android.ichooseyou
 
 import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
 import android.widget.Button
+import android.widget.VideoView
 import androidx.appcompat.app.AppCompatActivity
 import com.android.ichooseyou2.R
 
@@ -10,6 +12,16 @@ class OpeningPageActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.openingpage)
+
+        // Play video
+        val videoView: VideoView = findViewById(R.id.videoView)
+        val videoPath = "android.resource://" + packageName + "/" + R.raw.pokeballzrollin
+        val uri = Uri.parse(videoPath)
+        videoView.setVideoURI(uri)
+        videoView.start()
+
+        // Loop the video
+        videoView.setOnCompletionListener { videoView.start() }
 
         // Find buttons by ID
         val loginButton: Button = findViewById(R.id.button)
